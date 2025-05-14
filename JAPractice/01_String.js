37d595a6f1282440cdc2a9f2bc1edc57c8b3d806
vivekglbajaj <143469854+vivekglbajaj@users.noreply.github.com>
2025-05-14 07:41:48 +0000

    this is the day of Numbers and Math in javascript

---
+++ JAPractice/01_String.js
@@ -0,0 +1,18 @@
+
+const Name = "vivek"
+let age = 20
+console.log(`My Name is ${Name} and i am ${age} year old`);
+
+
+
+
+
+const newString = "   vivek    "
+console.log(newString);
+
+console.log(newString.trim());
+
+const url = "https://vivek.com/vivek%20yadav"
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('yadv'))
